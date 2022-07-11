eb67c4c22572783f9a7f4263cb94fa61e70186a6
derekhargest <derekhargest@gmail.com>
2022-07-11 12:54:23 -0400

    testing entrypoint - running from actions file

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,8 +16,9 @@ if [ -f "$package_path" ]
 then
 	echo "NVM"
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-	export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 	nvm install 14
 	nvm use 14
 	echo "Initiating NPM Install"
