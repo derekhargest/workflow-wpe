8719eac1c798cdf1701f8be4157692c8e91b2163
derekhargest <derekhargest@gmail.com>
2022-07-11 18:14:47 -0400

    seeing something

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ set -e
 #Alias logic for ENV names 
 if [[ -n ${WPE_ENV} ]]; then
     WPE_ENV_NAME="${WPE_ENV}";
-  elif [[ -n ${INPUT_PRD_ENV} ]]; then
-    WPE_ENV_NAME="${INPUT_PRD_ENV}";
-  elif [[ -n ${INPUT_STG_ENV} ]]; then
-    WPE_ENV_NAME="${INPUT_STG_ENV}";
-  elif [[ -n ${INPUT_DEV_ENV} ]]; then  
-    WPE_ENV_NAME="${INPUT_DEV_ENV}";
+  elif [[ -n ${PRD_ENV} ]]; then
+    WPE_ENV_NAME="${PRD_ENV}";
+  elif [[ -n ${STG_ENV} ]]; then
+    WPE_ENV_NAME="${STG_ENV}";
+  elif [[ -n ${DEV_ENV} ]]; then  
+    WPE_ENV_NAME="${DEV_ENV}";
   else echo "Failure: Missing environment variable..."  && exit 1;
 fi
 
@@ -21,7 +21,7 @@ echo "Deploying ${GITHUB_REF} to ${WPE_ENV_NAME} ..."
 # Deploy Vars
 WPE_SSH_HOST="$WPE_ENV_NAME.ssh.wpengine.net"
 DIR_PATH="$REMOTE_PATH"
-SRC_PATH="$INPUT_SRC_PATH"
+SRC_PATH="$SRC_PATH"
  
 # Set up our user and path
 WPE_SSH_USER="$WPE_ENV_NAME"@"$WPE_SSH_HOST"
@@ -57,7 +57,7 @@ find $SRC_PATH -type f -exec chmod -R 664 {} \;
 echo "file perms set..."
 
 # pre deploy php lint
-if [ "${INPUT_PHP_LINT^^}" == "TRUE" ]; then
+if [ "${PHP_LINT^^}" == "TRUE" ]; then
     echo "Begin PHP Linting."
     for file in $(find $SRC_PATH/ -name "*.php"); do
         php -l $file
@@ -72,8 +72,8 @@ else
 fi
 
 # post deploy script 
-if [[ -n ${INPUT_SCRIPT} ]]; then 
-    SCRIPT="&& sh ${INPUT_SCRIPT}";
+if [[ -n ${SCRIPT} ]]; then 
+    SCRIPT="&& sh ${SCRIPT}";
   else 
     SCRIPT=""
 fi 
@@ -92,20 +92,20 @@ ssh -nNf -v -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o Contr
 
 echo "!!! MASTER SSH CONNECTION ESTABLISHED !!!"
 #rsync 
-rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o 'ControlPath=$SSH_PATH/ctl/%C'" $INPUT_FLAGS --exclude-from='./exclude.txt' $SRC_PATH "$WPE_DESTINATION"
+rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o 'ControlPath=$SSH_PATH/ctl/%C'" $FLAGS --exclude-from='./exclude.txt' $SRC_PATH "$WPE_DESTINATION"
 
 # post deploy script and cache clear
 if [[ -n ${SCRIPT} || -n ${CACHE_CLEAR} ]]; then
 
     if [[ -n ${SCRIPT} ]]; then
-      if ! ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o ControlPath="$SSH_PATH/ctl/%C" $WPE_FULL_HOST "test -s sites/${WPE_ENV_NAME}/${INPUT_SCRIPT}"; then
+      if ! ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o ControlPath="$SSH_PATH/ctl/%C" $WPE_FULL_HOST "test -s sites/${WPE_ENV_NAME}/${SCRIPT}"; then
         status=1
       fi
 
-      if [[ $status -ne 0 && -f ${INPUT_SCRIPT} ]]; then
-        ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o ControlPath="$SSH_PATH/ctl/%C" $WPE_FULL_HOST "mkdir -p sites/${WPE_ENV_NAME}/$(dirname ${INPUT_SCRIPT})"
+      if [[ $status -ne 0 && -f ${SCRIPT} ]]; then
+        ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o ControlPath="$SSH_PATH/ctl/%C" $WPE_FULL_HOST "mkdir -p sites/${WPE_ENV_NAME}/$(dirname ${SCRIPT})"
 
-        rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o 'ControlPath=$SSH_PATH/ctl/%C'" ${INPUT_SCRIPT} wpe_gha+"$WPE_SSH_USER":sites/"$WPE_ENV_NAME"/$(dirname ${INPUT_SCRIPT})
+        rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o 'ControlPath=$SSH_PATH/ctl/%C'" ${SCRIPT} wpe_gha+"$WPE_SSH_USER":sites/"$WPE_ENV_NAME"/$(dirname ${SCRIPT})
       fi
     fi
 
