3279824d902b07ffefeaaac09e3d130ca2c874bd
derekhargest <derekhargest@gmail.com>
2022-07-11 12:47:42 -0400

    testing entrypoint - running from actions file

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,12 @@ cd ${working_directory:-./}
 # If we have composer dependencies make sure they are installed
 if [ -f "$package_path" ]
 then
-	echo "Package File found. Starting build."
-	composer install
+	echo "NVM"
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+	export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+	nvm install 14
+	nvm use 14
 	echo "Initiating NPM Install"
 	npm install
 	echo "Building"
