caf2031c730f970a6358e1a8704dcba265cc133f
derekhargest <derekhargest@gmail.com>
2022-07-20 11:25:48 -0400

    adding env variables

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 
 ?>
 <section class="content container container--padded container--max-width">
-	<h2>This is the DEV environment</h2>
 	<ol class="posts">
 		<?php while ( have_posts() ) : ?>
 			<?php the_post(); ?>
